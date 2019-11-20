9f735957cba72d5ca57a88a057fef456475013a1
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 16:56:35 -0500

    Clase 12: Consola

---
+++ modulos/consola.js
@@ -0,0 +1,32 @@
+// Console
+
+// console.log()
+// console.info()
+// console.warn()
+// console.error()
+
+// console.table()
+// console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 }]);
+// ┌─────────┬─────┬─────┐
+// │ (index) │  a  │  b  │
+// ├─────────┼─────┼─────┤
+// │    0    │  1  │ 'Y' │
+// │    1    │ 'Z' │  2  │
+// └─────────┴─────┴─────┘
+
+// console.group();
+// console.groupEnd();
+
+// EJEMPLO 1
+// console.group('despedida');
+// 	console.log('adios');
+// 	console.group();
+// 		console.log('Carlos');
+// 	console.groupEnd();
+// console.groupEnd();
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
