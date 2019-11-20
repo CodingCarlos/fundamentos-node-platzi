4b18411c8b7befcab23075fcb03ba7c193d17f60
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 16:30:39 -0500

    Clase 10: Modulos globales

---
+++ modulos/globales.js
@@ -0,0 +1,20 @@
+// Ubicación de ficheros
+// console.log(__dirname);
+// console.log(__filename);
+
+// Timers
+// console.log(setInterval);)
+// console.log(clearInterval);
+// console.log(setTimeout);
+// console.log(clearTimeout);
+// console.log(setInmediate);
+
+// Modules
+// console.log(exports);
+// console.log(module);
+// console.log(require);
+
+// Seteando variables globales
+// global.test = 'Ejemplo';
+// console.log(test);
+// console.log(global.test);
