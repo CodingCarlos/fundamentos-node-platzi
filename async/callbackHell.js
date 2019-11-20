70b3257d9b34d705add8c3ec6708d97ea339fa33
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 15:45:55 -0500

    Clase 7: Callback Hell

---
+++ async/callbackHell.js
@@ -0,0 +1,52 @@
+function hola(nombre, miCallback) {
+    setTimeout(function () {
+        console.log('Hola, '+ nombre);
+        miCallback(nombre);
+    }, 1500);
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function() {
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios', nombre);
+        otroCallback();
+    }, 1000);
+}
+
+function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+        hablar(function () {
+            conversacion(nombre, --veces, callback);
+        })
+    } else {
+        adios(nombre, callback);
+    }
+}
+
+// --
+
+console.log('Iniciando proceso...');
+hola('Carlos', function (nombre) {
+    conversacion(nombre, 3, function() {
+        console.log('Proceso terminado');
+    });
+});
+
+
+// hola('Carlos', function (nombre) {
+//     hablar(function () {
+//         hablar(function () {
+//             hablar(function () {
+//                 adios(nombre, function() {
+//                     console.log('Terminando proceso...');
+//                 });
+//             });
+//         });
+//     });
+// });
