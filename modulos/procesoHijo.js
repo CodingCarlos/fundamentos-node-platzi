fd62b79fd7174aaea207973ac01ea9595d25e11a
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 17:23:01 -0500

    Clase 14: Procesos hijo

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,26 @@
+const { exec, spawn } = require('child_process');
+
+// exec('node modulos/consola.js', (err, stdout, sterr) => {
+//     if (err) {
+//         console.error(err);
+//         return false;
+//     }
+
+//     console.log(stdout);
+// })
+
+let proceso = spawn('ls', ['-la']);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function (dato) {
+    console.log('¿Está muerto?');
+    console.log(proceso.killed);
+    console.log(dato.toString())
+});
+
+proceso.on('exit', function() {
+    console.log('el proeso terminó');
+    console.log(proceso.killed)
+})
