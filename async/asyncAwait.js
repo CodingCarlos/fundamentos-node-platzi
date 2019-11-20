0dfc5ee273cc232db72525a7fc69281d9afe944a
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 16:17:05 -0500

    Clase 9: Async / Await

---
+++ async/asyncAwait.js
@@ -0,0 +1,41 @@
+async function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola, '+ nombre);
+            resolve(nombre);
+        }, 1500);
+    });
+    
+}
+
+async function hablar(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function() {
+            console.log('Bla bla bla bla...');
+            //resolve(nombre);
+            resolve('Hay un error');
+        }, 1000);
+    });
+}
+
+async function adios(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function() {
+            console.log('Adios', nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+async function main() {
+    let nombre = await hola('Carlos');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Termina el proceso');
+}
+
+console.log('Empezamos el proceso');
+main();
+console.log('Va a ser la segunda instrucción')
