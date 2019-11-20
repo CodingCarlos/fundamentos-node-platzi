dea8914668a7c654d13ac6474dcb2c1288a2bb66
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 14:52:58 -0500

    Clase 3: Monohilo

---
+++ conceptos/monohilo.js
@@ -0,0 +1,13 @@
+console.log('Hola mundo');
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+
+    // if (i === 5) {
+    //     console.log('forzamos error');
+    //     var a = 3 + z;
+    // }
+}, 1000);
+
+console.log('Segunda instrucción');
