780a89e6269a55461e62c8430a8264bdb61ce05e
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-20 16:06:16 -0500

    Clase 8: Promesas

---
+++ async/promises.js
@@ -0,0 +1,45 @@
+function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola, '+ nombre);
+            resolve(nombre);
+        }, 1500);
+    });
+    
+}
+
+function hablar(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function() {
+            console.log('Bla bla bla bla...');
+            //resolve(nombre);
+            reject('Hay un error');
+        }, 1000);
+    });
+}
+
+function adios(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function() {
+            console.log('Adios', nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+// ---
+
+console.log('Iniciando el proceso..');
+hola('Carlos')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminado el proceso');
+    })
+    .catch(error => {
+        console.error('Ha habido un error:');
+        console.error(error);
+    })
