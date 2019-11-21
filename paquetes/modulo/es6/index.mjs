0d301c80e4c78d354ddea4329d0d762d5fff6a89
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-21 15:54:12 -0500

    Clase 20: Modules

---
+++ paquetes/modulo/es6/index.mjs
@@ -0,0 +1,4 @@
+import modulo from './modulo.mjs'
+
+console.log(modulo.prop1);
+modulo.saludar();
---
+++ paquetes/modulo/es6/modulo.mjs
@@ -0,0 +1,8 @@
+function saludar() {
+    console.log('Hola mundo!!');
+}
+
+export default {
+    saludar,
+    prop1: "Soy un modulo experimental",
+};
