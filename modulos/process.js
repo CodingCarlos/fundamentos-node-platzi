4ddccfb5ae6ecfd2e202c1828e06baa891dc6af2
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-21 15:04:56 -0500

    Clase 18: Process

---
+++ modulos/process.js
@@ -0,0 +1,28 @@
+// const p = require('process');
+
+process.on('beforeExit', () => {
+    console.log('el proceso va a terminar');
+});
+
+process.on('exit', () => {
+    console.log('Ale, el proceso acabó');
+    setTimeout(() => {
+        console.log('Esto no se va a ver nunca');
+    }, 0);
+});
+
+setTimeout(() => {
+    console.log('Esto se va a ver');
+}, 0);
+
+process.on('uncaughtException', (err, origen) => {
+    console.error('Vaya se nos ha olvidado capturar un error');
+    setTimeout(() => {
+        console.log('Esto viene desde las excepciones');
+    }, 0);
+});
+
+
+funcionQueNoExiste();
+
+console.log('Esto si el error no se recoje, no sale');
